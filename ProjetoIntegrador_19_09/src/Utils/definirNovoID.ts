fa9b2a895fca345447b4395475191e18e6b06bb5
VithorLeonardoMS <162487463+VithorLeonardoMS@users.noreply.github.com>
2024-09-19 17:10:21 -0300

    Add files via upload

---
+++ ProjetoIntegrador_19_09/src/Utils/definirNovoID.ts
@@ -0,0 +1,37 @@
+import { Comentario } from "../model/Comentario";
+import { Aula } from "../model/Postagem/Aula";
+import { CursoExterno } from "../model/Postagem/CursoExterno";
+import { CursoInterno } from "../model/Postagem/CursoInterno";
+import { Postagem } from "../model/Postagem/Postagem";
+import { Resposta } from "../model/Resposta";
+import { Usuario } from "../model/Usuario";
+import { IDsRemovidos } from "./Variaveis";
+
+
+
+    function definirNovoID(lista:Comentario[] | Resposta[] | Usuario[] | Aula[] | CursoExterno[] | CursoInterno[] | Postagem[], idsRemovidos?:number):number{
+
+        if(lista instanceof Comentario && idsRemovidos){
+            return lista.length + idsRemovidos
+        } else if(lista[0] instanceof Resposta && idsRemovidos){
+            return lista.length + idsRemovidos
+        } else if(lista[0] instanceof Usuario){
+            let retorno = IDsRemovidos.get('Usuarios')
+            return (retorno? retorno:0) + lista.length
+        } else if(lista[0] instanceof Aula){
+            let retorno = IDsRemovidos.get('Aula')
+            return (retorno? retorno:0) + lista.length
+        } else if(lista instanceof Usuario){// ver se lista é analizada corretamente pois é uma array
+            let retorno = IDsRemovidos.get('Usuarios')
+            return (retorno? retorno:0) + lista.length  
+        } else if(lista[0] instanceof Usuario){
+            let retorno = IDsRemovidos.get('Usuarios')
+            return (retorno? retorno:0) + lista.length
+        } else if(lista[0] instanceof Usuario){
+            let retorno = IDsRemovidos.get('Usuarios')
+            return (retorno? retorno:0) + lista.length
+        } else{
+            return -1
+        }
+
+    }
---
+++ ProjetoIntegrador_19_09/tests/Maptest.ts
@@ -0,0 +1,4 @@
+
+let IDLikesDados:any = new Map<string,number>
+
+IDLikesDados.set('Comentario', 1)
